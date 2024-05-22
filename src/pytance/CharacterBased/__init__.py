d0ec09ccd6be4940e0a37cfe13dd7c9306b42314
MohammadMahdi Farahbakhsh <mahdi.farhbakhsh99@gmail.com>
2024-05-22 19:08:24 +0330

    CharacterBased.dist("word1",word2,"jaro"

--- src/pytance/CharacterBased/__init__.py
+++ src/pytance/CharacterBased/__init__.py
@@ -1,4 +1,19 @@
 
-from .functions import hamming_distance, damerau_levenshtein_distance, jaro_distance, levenshtein_distance,jaro_winkler_distance
+from .utills import hamming_distance, damerau_levenshtein_distance, jaro_distance, levenshtein_distance,jaro_winkler_distance
+# from . import utills
 
 __all__ = ["hamming_distance", "damerau_levenshtein_distance", "jaro_distance", "levenshtein_distance", "jaro_winkler_distance"]
+
+functions = {
+          "hamming": hamming_distance,
+          "damerau_levenshtein": damerau_levenshtein_distance,
+          "jaro": jaro_distance,
+          "levenshtein": levenshtein_distance,
+          "jaro_winkler": jaro_winkler_distance
+        }
+
+def dist(word1, word2, function_name = 'hamming'):
+    if function_name in functions:
+        return functions[function_name](word1, word2)
+    else:
+        raise ValueError(f"No such function: {function_name}")
